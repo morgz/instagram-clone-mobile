f2484008619a1c03a134084f1a168b49b1c192e5
Daniel Morgan <danmorgz@gmail.com>
2018-02-22 23:03:10 +0000

    add auth to request

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,14 +1,37 @@
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { AsyncStorage } from 'react-native';
+import { authToken } from '../utils/constants';
+
+let token;
+
+const getToken = async () => {
+  if (token != null) {
+    return token;
+  }
+
+  token = AsyncStorage.getItem(authToken);
+  return token;
+};
 
 const cache = new InMemoryCache();
-
-const link = new HttpLink({
+const httpLink = createHttpLink({
   uri: 'http://192.168.0.19:4000/api/graphql',
 });
 
+const authLink = setContext(async (_, { headers }) => {
+  await getToken();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
 export const client = new ApolloClient({
-  link,
+  link: authLink.concat(httpLink),
   cache,
 });
