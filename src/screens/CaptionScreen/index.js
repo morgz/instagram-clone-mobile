02d463b8a45eac45ea5e4c6fe410dc9c4abe4f73
Daniel Morgan <danmorgz@gmail.com>
2018-02-27 18:46:30 +0000

    uploading photo and dismiss

--- src/screens/CaptionScreen/index.js
+++ src/screens/CaptionScreen/index.js
@@ -1,13 +1,33 @@
 import React, { PureComponent } from 'react';
-import { Text, View, StyleSheet, Image, TextInput, Keyboard } from 'react-native';
+import { Text, View, StyleSheet, Image, TextInput, Keyboard, ActivityIndicator } from 'react-native';
+import Touchable from '@appandflow/touchable';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import gql from 'graphql-tag';
+import { withApollo, graphql } from 'react-apollo';
+
 import { colors } from '../../utils/themes';
 import { Divider } from '../../components';
-import Touchable from '@appandflow/touchable';
+import { uploadImageToS3 } from '../../utils/uploadImage';
+import { createPhotoMutation } from '../../graphql/mutations/';
+import { FeedsPhotoFragment } from '../../screens/FeedsScreen/fragments';
+
+const signS3Query = gql`
+  query {
+      presignUrl {
+        uploadUrl
+        url
+      }
+  }
+`;
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  loadingWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   header: {
     height: 150,
@@ -44,13 +64,68 @@ const styles = StyleSheet.create({
 });
 
 class CaptionScreen extends PureComponent {
-  state = {
-    caption: '',
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      caption: '',
+      loading: false,
+    };
+
+    props.navigator.setOnNavigatorEvent(this._onNavigatorEvent.bind(this));
+  }
+
+  componentDidMount() {
+    this.props.navigator.setButtons({
+      rightButtons: [
+        {
+          id: 'sharePost',
+          title: 'Share',
+        },
+      ],
+      animated: true,
+    });
+  }
+
+  _onNavigatorEvent = e => {
+    if (e.type === 'NavBarButtonPress') {
+      if (e.id === 'sharePost') {
+        this._onSharePostPress();
+      }
+    }
+  }
+
+  _onSharePostPress = async () => {
+    this.setState({ loading: true });
+    const res = await this.props.client.query({ query: signS3Query });
+
+    const resultFromS3 = await uploadImageToS3(
+      this.props.image.node.image.uri,
+      res.data.presignUrl,
+    );
+
+    await this.props.onCreatePhoto({
+      imageUrl: resultFromS3.remoteUrl,
+      caption: this.state.caption,
+    });
+
+    this.setState({ loading: false });
+    this.props.navigator.dismissModal({
+      animationType: 'slide-down',
+    });
   }
 
   _onCaptionChange = text => this.setState({ caption: text });
 
   render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.loadingWrapper}>
+          <ActivityIndicator size='large' />
+        </View>
+      );
+    }
+
     return (
       <Touchable
         feedback="none"
@@ -93,4 +168,30 @@ class CaptionScreen extends PureComponent {
   }
 }
 
-export default CaptionScreen;
+const getPhotos = gql`
+  query {
+    photos {
+      ...feedsPhoto
+    }
+  }
+  ${FeedsPhotoFragment}
+`;
+
+export default graphql(createPhotoMutation, {
+  props: ({ mutate }) => ({
+    onCreatePhoto: variables =>
+      mutate({
+        variables,
+        update: (store, { data: { createPhoto } }) => {
+          const query = store.readQuery({ query: getPhotos });
+
+          store.writeData({
+            query: getPhotos,
+            data: {
+              photos: [createPhoto, ...query.photos],
+            },
+          });
+        },
+      }),
+  }),
+})(withApollo(CaptionScreen));
